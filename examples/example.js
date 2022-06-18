46aa27c72488ba29793e441b85ef8433043cf836
cactoes <Maartenhvanrooij@gmail.com>
2022-06-18 17:58:34 +0200

    updated example

--- examples/example.js
+++ examples/example.js
@@ -2,7 +2,7 @@
 const { client, C_Game, C_User, C_Runes } = require("lcinterface") // import for the interfaces
 
 // interfaces for interacting with the game
-const c_user = new C_User()
+const c_user = new C_User({canCallUnhooked: false})
 
 // start a soon as we have the credentials
 client.on("connect", async (game_data) => {
@@ -12,7 +12,7 @@ client.on("connect", async (game_data) => {
   // if we hooked successfully do ...
   if (c_user.isCorrectState("hooked", true)) {
     // "virtually" call a league endpoint with get, post, put, delete
-    const user_data = await c_user.virtualCall(c_user.dest.me)
+    const user_data = await c_user.virtualCall(c_user.dest.me, undefined, "get")
 
     // log the user data we got from the client
     console.log(user_data)
